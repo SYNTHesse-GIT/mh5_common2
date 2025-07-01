576f99c59a88b862d4d2172eb4cb92cb26554cd6
sonelu <alex.sonea@gmail.com>
2025-07-01 11:41:30 +0100

    parametetric launch file fixed version in readme

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 These packages for ROS2 can be installed on device (robot SOC) as well as on a remote computer.
 
-[![Version](https://img.shields.io/badge/Rev-F-blue)](https://img.shields.io/badge/master-0.2.0-blue)
+[![Version](https://img.shields.io/badge/Rev-H-blue)](https://img.shields.io/badge/master-0.2.0-blue)
 
 They include:
 - `mh5_description`: includes the URDF model of MH5 and a few launch scripts for RVIZ that allows the visualization of the robot, with or without a  `joint_state_publisher_gui` node
--- mh5_description/launch/display.launch.py
+++ mh5_description/launch/display.launch.py
@@ -1,4 +1,7 @@
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration, EqualsSubstitution
 from launch_ros.parameter_descriptions import ParameterValue
 from launch_ros.actions import Node
 from launch.substitutions import Command
@@ -7,16 +10,36 @@ from ament_index_python.packages import get_package_share_path
 
 def generate_launch_description():
 
+    urdf_path = os.path.join(get_package_share_path('mh5_description'),
+                             'urdf', 'mh5_robot_revH.urdf.xacro')
     rviz_config_path = os.path.join(get_package_share_path('mh5_description'),
                                     'rviz', 'urdf_config.rviz')
     
+    robot_description = ParameterValue(Command(['xacro ', urdf_path]), value_type=str)
 
-    rviz2_node = Node(
-        package="rviz2",
-        executable="rviz2",
-        arguments=['-d', rviz_config_path]
-    )
-
+    
     return LaunchDescription([
-        rviz2_node
+        DeclareLaunchArgument(
+            name="use_gui",
+            default_value="true",
+            description="start joint_state_publisher_gui_node"
+        ),
+
+        Node(
+            package="robot_state_publisher",
+            executable="robot_state_publisher",
+            parameters=[{'robot_description': robot_description}]
+        ),
+
+        Node(
+            package="joint_state_publisher_gui",
+            executable="joint_state_publisher_gui",
+            condition=IfCondition(EqualsSubstitution(LaunchConfiguration("use_gui"), "true"))
+        ),
+
+        Node(
+            package="rviz2",
+            executable="rviz2",
+            arguments=['-d', rviz_config_path]
+        )
     ])
